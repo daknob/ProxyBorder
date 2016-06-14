eeeadb2667e52e062923b5f731d4a3ccd922f40a
DaKnOb <daknob.mac@gmail.com>
2016-06-14 16:29:57 +0300

    Rewrite Proxy Border

--- proxyborder.py
+++ proxyborder.py
@@ -1,102 +1,81 @@
-#!usr/bin/python
-#ProxyBorder By DaKnOb
+#!/usr/bin/python
 
-import urllib2	# Makes the tool work
-import thread	# Makes the tool faster
-import random	# Makes the tool evenly distributed 
-import time	# Makes the tool friendlier
-import string	# Makes the tool easier
-import sys	# Makes the tool expandable
+# Proxy Border by DaKnOb
+# Complete rewrite
 
-def getURLtoMakeRequest():
-	
-	#	Return a URL that is up most if not
-	#	all of the time to request with a
-	#	specific proxy that has to be tested.
+import requests
+import threading
+from random import choice
+import sys
 
-        urls = []
-        urls.append("http://www.google.com")
-        urls.append("http://www.youtube.com")
-	urls.append("http://www.amazon.com")
-        urls.append("http://www.apple.com")
-        urls.append("http://www.microsoft.com")
-        urls.append("http://www.samsung.com")
-        urls.append("http://www.facebook.com")
-        urls.append("http://www.twitter.com")
-        urls.append("http://www.yahoo.com")
+AlwaysOn = [
+	"http://www.google.com",
+	"http://www.youtube.com",
+	"http://www.amazon.com",
+	"http://www.apple.com",
+	"http://www.microsoft.com",
+	"http://www.samsung.com",
+	"http://www.facebook.com",
+	"http://www.twitter.com",
+	"http://www.yahoo.com",
+	"http://www.wikipedia.org",
+	"http://www.akamai.com",
+	"http://www.cloudflare.com"
+]
 
-        return urls[random.randrange(0,len(urls) - 1)]
+def getRandomURL():
+	return choice(AlwaysOn)
 
-def checkProxyFromList(ipcheck, *args):
-
-	#	Check if you can connect to a
-	#	proxy and request a website from
-	#	it. Then write this to the working
-	#	proxy list.
-
-	global toCheck
-
-        try:
-                proxy_support = urllib2.ProxyHandler({"http":str(ipcheck)})
-                opener = urllib2.build_opener(proxy_support) 
-                urllib2.install_opener(opener) 
-                urllib2.urlopen(str(getURLtoMakeRequest())).read() 
-
-		#	We don't care about 404 / 300, etc. 
-		#	Only successful connection
-                n = file(savefile, "a+")
-                n.write(ipcheck + "\n")
-                n.close()
-
-		#	We let the OS handle multiple
-		#	writes to the same file by
-		#	different threads.
-
-        except:
-                pass
-
-	toCheck = toCheck - 1
-
-def startThreadWithProxyToCheck(proxii):
-
-	#	Start a new Python thread to check
-	#	a proxy from the list inserted
-
+def checkProxy(ip, port):
 	try:
-                thread.start_new_thread(checkProxyFromList,(proxii,random.randrange(0,9000)))
-        except:
-                time.sleep(1)
-                startThreadWithProxyToCheck(proxii)
-
-if(len(sys.argv) != 3):
-	proxyfile = raw_input("Enter the path of the proxies to check: ")
-	savefile = raw_input("Enter the path to make the new proxy list: ")
-else:
-	try:
-		proxyfile = sys.argv[1]
-		savefile = sys.argv[2]
+		requests.get(
+			getRandomURL(), 
+			proxies = {'http': 'http://' + ip + ':' + str(port)},
+			timeout = 5
+		)
 	except:
-		print ("Usage: python proxyborder.py check-these.txt save-here.txt")
-		exit(5)
+		writeNotWorkingProxyToOutput(ip, port)
+		return
+	writeWorkingProxyToOutput(ip, port)
+	return
 
+def writeWorkingProxyToOutput(ip, port):
+	with open(Working, "a+") as out:
+		out.write(ip + ":" + str(port) + "\n")
+	return
 
+def writeNotWorkingProxyToOutput(ip, port):
+	with open(NotWorking, "a+") as out:
+		out.write(ip + ":" + str(port) + "\n")
+	return
+
+# Main
 try:
-        h = file(savefile, "w+")
+	Input = sys.argv[1]
+	Working = sys.argv[2]
+	NotWorking = sys.argv[3]
+	MaxThreads = int(sys.argv[4])
 except:
-        print "Could not open file to save working proxies."
-        exit(1)
+	print("Usage:")
+	print("")
+	print("proxyborder.py input.txt working.txt notworking.txt 32")
+	print("\tinput.txt: A list of newline delimited proxies in the form of IP:PORT")
+	print("\tworking.txt: The output file to append working proxies. Same format.")
+	print("\tnotworking.txt: The output filr to append nonworking proxies. Same format.")
+	print("\t32: The maximum amount of threads to spawn to check the proxy list.")
+	print("Caveats:")
+	print("\tSome proxies may appear as non-working because of slow response times.")
+	print("\tWe suggest you run proxyborder again with the notworking.txt as input")
+	print("\tand working.txt as output.")
+	exit(1)
 
-import fileinput
-ProxiesToCheck = []
-for line in fileinput.input([proxyfile]):
-        ProxiesToCheck.append(string.replace(line, "\n", ""))
-
-toCheck = len(ProxiesToCheck)
-
-for px in ProxiesToCheck:
-        startThreadWithProxyToCheck(px)
-
-
-while(toCheck > 0):
-	pass
-
+with open(Input, "r") as proxies:
+	for proxy in proxies:
+		p = proxy.replace("\n", "")
+		p = p.split(":")
+		while(threading.activeCount() >= MaxThreads):
+			pass
+		try:
+			threading.Thread(target=checkProxy, args=(p[0], p[1])).start()
+		except:
+			print("Invalid Line:\t" + proxy)
